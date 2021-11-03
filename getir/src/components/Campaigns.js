2fc94f11e8a1f094043af6b3394f9f25706f2688
brkeozbk <brkeozbk@gmail.com>
2021-11-03 19:43:31 +0300

    update

--- getir/src/components/Campaigns.js
+++ getir/src/components/Campaigns.js
@@ -1,8 +1,38 @@
-
+import Slider from "react-slick";
+import Banners from 'api/banner.json'
+import { useState, useEffect } from "react";
+import Title from "./ui/Title";
 export default function Header() {
+    const [banners, setBanners] = useState([]);
+
+    useEffect(() => {
+        setBanners(Banners);
+        
+    }, [])
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        arrows:false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+      };
+
     return (
-        <div>
-            Header
+        <div className="container mx-auto">
+            <Title> Kapmanya </Title>
+           <Slider {...settings}>
+       {banners.length && banners.map((banner, index)=>(
+           <div key={banner.id}>
+               <img src={banner.image} />
+           </div>
+       ))}
+      </Slider>
         </div>
     )
 }
